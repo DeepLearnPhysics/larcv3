dc39a68091fc54f2d204244a00b35d6a088b640e
Corey Adams <corey.adams@anl.gov>
2019-03-25 10:42:06 -0500

    Update for more verbosity

--- tests/test_swig.py
+++ tests/test_swig.py
@@ -1,5 +1,12 @@
 import unittest
 
+def test_fake():
+    import os
+    print(os.environ)
+    import larcv
+    print(os.environ)
+    return False
+    
 def test_import():
     import larcv
     from larcv import base
