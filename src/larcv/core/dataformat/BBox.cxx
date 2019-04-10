3499196c1f56e676442a16e2811c8713d3619ab5
Corey Adams <corey.adams@anl.gov>
2019-04-10 15:38:27 -0500

    Templated version of BBox Compiles

--- src/larcv/core/dataformat/BBox.cxx
+++ src/larcv/core/dataformat/BBox.cxx
@@ -5,161 +5,128 @@
 #include <cmath>
 #include <set>
 #include <sstream>
-#include "larcv/core/Base/larbys.h"
+#include "core/base/larbys.h"
 
 namespace larcv {
 
-  BBox2D::BBox2D(double xmin, double ymin, double xmax, double ymax, ProjectionID_t id)
-    : _id(id), _p1(xmin,ymin) , _p2(xmax,ymax)
+  template<size_t dimension>
+  BBox<dimension>::BBox(Point<dimension> min, Point<dimension> max, ProjectionID_t id )
+    : _id(id), _p1(min), _p2(max)
   {
-    if(xmin > xmax) throw larbys("xmin > xmax not allowed for BBox2D construction!");
-    if(ymin > ymax) throw larbys("ymin > ymax not allowed for BBox2D construction!");
+    for (size_t i = 0; i < dimension; i++){
+      if(_p1.x[i] > _p2.x[i]) throw larbys("min > max not allowed for BBox construction!");
+    }
   }
 
-  void BBox2D::update(double xmin, double ymin, double xmax, double ymax, ProjectionID_t id)
+  template<size_t dimension>
+  void BBox<dimension>::update(const std::vector<double> & min, const std::vector<double> & max, ProjectionID_t id)
   {
-    if(xmin > xmax) throw larbys("xmin > xmax not allowed for BBox2D::Update!");
-    if(ymin > ymax) throw larbys("ymin > ymax not allowed for BBox2D::Update!");
-    _p1.x = xmin; _p1.y = ymin;
-    _p2.x = xmax, _p2.y = ymax;
+    if(min.size() != max.size() ||
+       min.size() != dimension){
+      throw larbys("Must use min and max of correct length for BBox update!");
+    }
+    for (size_t i = 0; i < dimension; i++){
+      if(min[i] > max[i]) throw larbys("min > max not allowed for BBox construction!");
+    }
+    for (size_t i = 0; i < dimension; i++){
+      _p1.x[i] = min[i];
+      _p2.x[i] = max[i];
+    }
     _id = id;
   }
 
-  void BBox2D::update(const Point2D& p1, const Point2D& p2, ProjectionID_t id)
-  { update(p1.x,p1.y,p2.x,p2.y); _id = id; }
+  template<size_t dimension>
+  void BBox<dimension>::update(const Point<dimension>& p1, const Point<dimension>& p2, ProjectionID_t id)
+  {    
 
-  void BBox2D::update(ProjectionID_t id)
+    for (size_t i = 0; i < dimension; i++){
+      if(p1.x[i] > p2.x[i]) throw larbys("min > max not allowed for BBox construction!");
+    }
+    for (size_t i = 0; i < dimension; i++){
+      _p1.x[i] = p1.x[i];
+      _p2.x[i] = p2.x[i];
+    }
+    _id = id;
+  }
+
+  template<size_t dimension>
+  void BBox<dimension>::update(ProjectionID_t id)
   { _id = id; }
 
-  BBox2D BBox2D::overlap(const BBox2D& box) const
+  template<size_t dimension>
+  BBox<dimension> BBox<dimension>::overlap(const BBox<dimension>& box) const
   {
-    double minx = ( box.min_x() < this->min_x() ? this->min_x() : box.min_x()  ); //pick larger x min-bound
-    double maxx = ( box.max_x() < this->max_x() ? box.max_x()  : this->max_x() ); //pick smaller x max-bound
 
-    double miny = ( box.min_y() < this->min_y() ? this->min_y() : box.min_y()  ); //pick larger x min-bound
-    double maxy = ( box.max_y() < this->max_y() ? box.max_y()  : this->max_y() ); //pick smaller x max-bound
+    Point<dimension> overlap_p1;
+    Point<dimension> overlap_p2;
 
-    if(!(minx < maxx && miny < maxy)) {
-      std::stringstream ss;
-      ss << "No overlap found ... this X: " << this->min_x() << " => " << this->max_x() << " Y: " << this->min_y() << " => " << this->max_y()
-	 << " ... the other X: " << box.min_x() << " => " << box.max_x() << " Y: " << box.min_y() << " => " << box.max_y() << std::endl;
-      throw larbys(ss.str());
+    for (size_t i = 0; i < dimension; i ++){
+      overlap_p1.x[i] = std::max(this->_p1.x[i], box.min().x[i]); // Pick larger min coordinate
+      overlap_p2.x[i] = std::min(this->_p1.x[i], box.max().x[i]); // Pick smaller max coordinate
+      if (overlap_p2.x[i] < overlap_p1.x[i]){ // verify there is overlap
+              std::stringstream ss;
+        ss << "No overlap found along dimension " << i << ":\n" 
+           << "  this X: " << this->_p1.x[i] << " ... the other X: " << box.min().x[i] << std::endl;
+        throw larbys(ss.str());
+      }
     }
-    return BBox2D(minx, miny, maxx, maxy);
+
+    return BBox<dimension>(overlap_p1, overlap_p2);
   }
 
-  BBox2D BBox2D::inclusive(const BBox2D& box) const
+  template<size_t dimension>
+  BBox<dimension> BBox<dimension>::inclusive(const BBox<dimension>& box) const
   {
-    double min_x = ( box.min_x() < this->min_x() ? box.min_x() : this->min_x() ); //pick smaller x min-boudn
-    double max_x = ( box.max_x() > this->max_x() ? box.max_x() : this->max_x() ); //pick larger x max-bound
 
-    double min_y = ( box.min_y() < this->min_y() ? box.min_y() : this->min_y() ); //pick smaller y min-boudn
-    double max_y = ( box.max_y() > this->max_y() ? box.max_y() : this->max_y() ); //pick larger y max-bound
 
-    return BBox2D(min_x, min_y, max_x, max_y);
+    Point<dimension> overlap_p1;
+    Point<dimension> overlap_p2;
+
+    for (size_t i = 0; i < dimension; i ++){
+
+      overlap_p1.x[i] = std::min(this->_p1.x[i], box.min().x[i]); // Pick smaller min coordinate
+      overlap_p2.x[i] = std::max(this->_p1.x[i], box.max().x[i]); // Pick larger max coordinate
+    }
+
+    return BBox<dimension>(overlap_p1, overlap_p2);
+
   }
 
-  bool BBox2D::contains(const Point2D& point) const
+  template<size_t dimension>
+  bool BBox<dimension>::contains(const Point<dimension>& point) const
   {
-    return point.x >= this->min_x() && point.y >= this->min_y() && point.x <= this->max_x() && point.y <= this->max_y();
+    bool contained(true);
+    for (size_t i = 0; i < dimension; i ++){
+      contained = ( contained && point.x[i] >= _p1.x[i] && point.x[i] <= _p2.x[i]);
+    }
+    return contained;
   }
 
-  std::string BBox2D::dump() const
+  template<size_t dimension>
+  std::string BBox<dimension>::dump() const
   {
     std::stringstream ss;
-    ss << "    (" << _p1.x << "," << _p1.y << ") => (" << _p2.x << "," << _p2.y << ")" << std::endl;;
+    // Add the first point:
+    ss << "    (";
+    for (size_t i = 0; i < dimension; i ++){
+      ss << _p1.x[i];
+      if (i != dimension -1) ss << ",";
+    }
+
+    ss << ") => (";
+    // Add the second point:
+    for (size_t i = 0; i < dimension; i ++){
+      ss << _p2.x[i];
+      if (i != dimension -1) ss << ",";
+    }
+
+    ss << ")" << std::endl;;
     return ss.str();
   }
 
-  //
-  // BBox3D
-  //
 
-  BBox3D::BBox3D(double xmin, double ymin, double zmin,
-		 double xmax, double ymax, double zmax)
-    : _p1(xmin,ymin,zmin) , _p2(xmax,ymax,zmax)
-  {
-    if(xmin > xmax) throw larbys("xmin > xmax not allowed for BBox3D construction!");
-    if(ymin > ymax) throw larbys("ymin > ymax not allowed for BBox3D construction!");
-    if(zmin > zmax) throw larbys("zmin > zmax not allowed for BBox3D construction!");
-  }
-
-
-
-  void BBox3D::update(double xmin, double ymin, double zmin,
-		      double xmax, double ymax, double zmax)
-  {
-    if(xmin > xmax) throw larbys("xmin > xmax not allowed for BBox3D::Update!");
-    if(ymin > ymax) throw larbys("ymin > ymax not allowed for BBox3D::Update!");
-    if(zmin > zmax) throw larbys("zmin > zmax not allowed for BBox3D::Update!");
-    _p1.x = xmin; _p1.y = ymin; _p1.z = zmin;
-    _p2.x = xmax, _p2.y = ymax; _p2.z = zmax;
-  }
-
-  void BBox3D::update(const Point3D& p1, const Point3D& p2)
-  { update(p1.x,p1.y,p1.z,p2.x,p2.y,p2.z); }
-
-  double BBox3D::area(int axis) const
-  {
-    if(axis == 0) return (_p2.x - _p1.x) * (_p2.y - _p1.y); // xy projection
-    if(axis == 1) return (_p2.x - _p1.x) * (_p2.z - _p1.z); // xz projection
-    if(axis == 2) return (_p2.y - _p1.y) * (_p2.z - _p1.z); // yz projection
-    throw larbys("BBox3D::area can only take axis=[0,1,2]!");
-  }
-
-
-  BBox3D BBox3D::overlap(const BBox3D& box) const
-  {
-    double minx = ( box.min_x() < this->min_x() ? this->min_x() : box.min_x()  ); //pick larger x min-bound
-    double maxx = ( box.max_x() < this->max_x() ? box.max_x()  : this->max_x() ); //pick smaller x max-bound
-
-    double miny = ( box.min_y() < this->min_y() ? this->min_y() : box.min_y()  ); //pick larger x min-bound
-    double maxy = ( box.max_y() < this->max_y() ? box.max_y()  : this->max_y() ); //pick smaller x max-bound
-
-    double minz = ( box.min_z() < this->min_z() ? this->min_z() : box.min_z()  ); //pick larger x min-bound
-    double maxz = ( box.max_z() < this->max_z() ? box.max_z()  : this->max_z() ); //pick smaller x max-bound
-
-    if(!(minx < maxx && miny < maxy && minz < maxz)) {
-      std::stringstream ss;
-      ss << "No overlap found ... this"
-	 << " X: " << this->min_x() << " => " << this->max_x()
-	 << " Y: " << this->min_y() << " => " << this->max_y()
-	 << " Z: " << this->min_z() << " => " << this->max_z()
-	 << " ... the other"
-	 << " X: " << box.min_x() << " => " << box.max_x()
-	 << " Y: " << box.min_y() << " => " << box.max_y()
-	 << " Z: " << box.min_z() << " => " << box.max_z()
-	 << std::endl;
-      throw larbys(ss.str());
-    }
-    return BBox3D(minx, miny, minz, maxx, maxy, maxz);
-  }
-
-  BBox3D BBox3D::inclusive(const BBox3D& box) const
-  {
-    double min_x = ( box.min_x() < this->min_x() ? box.min_x() : this->min_x() ); //pick smaller x min-boudn
-    double max_x = ( box.max_x() > this->max_x() ? box.max_x() : this->max_x() ); //pick larger x max-bound
-
-    double min_y = ( box.min_y() < this->min_y() ? box.min_y() : this->min_y() ); //pick smaller y min-boudn
-    double max_y = ( box.max_y() > this->max_y() ? box.max_y() : this->max_y() ); //pick larger y max-bound
-
-    double min_z = ( box.min_z() < this->min_z() ? box.min_z() : this->min_z() ); //pick smaller z min-boudn
-    double max_z = ( box.max_z() > this->max_z() ? box.max_z() : this->max_z() ); //pick larger z max-bound
-
-    return BBox3D(min_x, min_y, min_z, max_x, max_y, max_z);
-  }
-
-  bool BBox3D::contains(const Point3D& point) const
-  {
-    return point.x >= this->min_x() && point.y >= this->min_y() && point.z >= this->min_z() && point.x <= this->max_x() && point.y <= this->max_y() && point.z <= this->max_z();
-  }
-
-  std::string BBox3D::dump() const
-  {
-    std::stringstream ss;
-    ss << "    (" << _p1.x << "," << _p1.y << "," << _p1.z << ") => (" << _p2.x << "," << _p2.y << "," << _p2.z << ")" << std::endl;;
-    return ss.str();
-  }
+template class BBox<2>;
+template class BBox<3>;
 
 }
 
--- src/larcv/core/dataformat/BBox.h
+++ src/larcv/core/dataformat/BBox.h
@@ -5,7 +5,7 @@
  *
  * \brief Class def header for a class larcv::BBox2D and larcv::BBox3D
  *
- * @author kazuhiro
+ * @author kazuhiro, cadams
  */
 
 /** \addtogroup core_DataFormat
@@ -20,109 +20,63 @@
 namespace larcv {
 
   /**
-     \class BBox2D
-     \brief Bounding box in 2D
+     \class BBox
+     \brief Bounding box in ND
   */
-  class BBox2D {
 
+  template<size_t dimension>
+  class BBox{
   public:
+    BBox(Point<dimension> min, Point<dimension> max, ProjectionID_t id = kINVALID_PROJECTIONID);    
+    
+    template<size_t other_dim>
+    inline bool operator== (const BBox<other_dim>& rhs) const {
+      assert(dimension == other_dim);
+      return (_p1 == rhs._p1 && _p2 == rhs._p2); 
+    }
 
-    /// Default constructor
-    BBox2D(double xmin = 0, double ymin = 0,
-           double xmax = 0, double ymax = 0,
-           ProjectionID_t id = kINVALID_PROJECTIONID);
-
-    /// Default destructor
-    virtual ~BBox2D() {}
-
-    inline bool operator== (const BBox2D& rhs) const
-    { return (_p1 == rhs._p1 && _p2 == rhs._p2); }
-
-    void update(double xmin, double ymin,
-                double xmax, double ymax,
+    void update(const std::vector<double> & min, const std::vector<double> & max,
                 ProjectionID_t id = kINVALID_PROJECTIONID);
-    void update(const Point2D& pt1, const Point2D& pt2,
+
+    void update(const Point<dimension>& pt1, const Point<dimension>& pt2,
                 ProjectionID_t id = kINVALID_PROJECTIONID);
+
     void update(ProjectionID_t id);
+
     inline bool empty() const { return (_p1 == _p2); }
-    inline const Point2D& origin      () const { return _p1; }
-    inline const Point2D& bottom_left () const { return _p1; }
-    inline const Point2D& top_right   () const { return _p2; }
-    inline Point2D center  () const { return Point2D(center_x(), center_y()); }
-    inline double center_x () const { return _p1.x + 0.5*(_p2.x - _p1.x); }
-    inline double center_y () const { return _p1.y + 0.5*(_p2.y - _p1.y); }
-    inline double min_x  () const { return _p1.x; }
-    inline double min_y  () const { return _p1.y; }
-    inline double max_x  () const { return _p2.x; }
-    inline double max_y  () const { return _p2.y; }
-    inline double width  () const { return _p2.x - _p1.x; }
-    inline double height () const { return _p2.y - _p1.y; }
-    inline double area   () const { return (_p2.y - _p1.y) * (_p2.x - _p1.x); }
+    inline const Point<dimension>& origin      () const { return _p1; }
+    inline const Point<dimension>& bottom_left () const { return _p1; }
+    inline const Point<dimension>& top_right   () const { return _p2; }
+    inline Point<dimension> center     () const { return (_p1 + _p2) * 0.5; }
+    inline Point<dimension> min        () const { return _p1;}
+    inline Point<dimension> max        () const { return _p1;}
+    inline Point<dimension> dimensions () const { return _p2 - _p1; }
+    inline double           area       () const { return volume(); }
+    inline double           volume     () const { 
+      double v = 1.0;
+      Point<dimension> res = _p2 - _p1;
+      for (size_t i = 0; i < dimension; i ++){
+        v *= res.x[i];
+      }
+      return v; 
+    }
+    
     inline ProjectionID_t id() const { return _id; }
     std::string dump() const;
 
-    BBox2D overlap(const BBox2D& box) const;
-    BBox2D inclusive(const BBox2D& box) const;
-    bool contains(const Point2D& point) const;
+    BBox overlap(const BBox& box) const;
+    BBox inclusive(const BBox& box) const;
+    bool contains(const Point<dimension>& point) const;
 
   private:
     ProjectionID_t _id; ///< ProjectionID_t identifies types of 2D projections to which BBox belongs
-    Point2D _p1; ///< bottom-left point coordinate (x1,y1) where x1<x2 and y1<y2
-    Point2D _p2; ///< top-right point coordinate (x2,y2) where x1<x2 and y1<y2
+    Point<dimension> _p1; ///< bottom-left point coordinate where x1<x2 and y1<y2, ...
+    Point<dimension> _p2; ///< top-right point coordinate where x1<x2 and y1<y2, ...
   };
 
-  /**
-     \class BBox3D
-     \brief Bounding box in 3D
-  */
-  class BBox3D {
+  typedef BBox<2> BBox2D;
+  typedef BBox<3> BBox3D;
 
-  public:
-
-    /// Default constructor
-    BBox3D(double xmin = 0, double ymin = 0, double zmin = 0,
-           double xmax = 0, double ymax = 0, double zmax = 0);
-
-    /// Default destructor
-    virtual ~BBox3D() {}
-
-    inline bool operator== (const BBox3D& rhs) const
-    { return (_p1 == rhs._p1 && _p2 == rhs._p2); }
-
-    void update(double xmin, double ymin, double zmin,
-                double xmax, double ymax, double zmax);
-
-    void update(const Point3D& pt1, const Point3D& pt2);
-    inline bool empty() const { return (_p1 == _p2); }
-    inline const Point3D& origin      () const { return _p1; }
-    inline const Point3D& bottom_left () const { return _p1; }
-    inline const Point3D& top_right   () const { return _p2; }
-    inline Point3D center  () const { return Point3D(center_x(), center_y(), center_z()); }
-    inline double center_x () const { return _p1.x + 0.5*(_p2.x - _p1.x); }
-    inline double center_y () const { return _p1.y + 0.5*(_p2.y - _p1.y); }
-    inline double center_z () const { return _p1.z + 0.5*(_p2.z - _p1.z); }
-    inline double min_x  () const { return _p1.x; }
-    inline double min_y  () const { return _p1.y; }
-    inline double min_z  () const { return _p1.z; }
-    inline double max_x  () const { return _p2.x; }
-    inline double max_y  () const { return _p2.y; }
-    inline double max_z  () const { return _p2.z; }
-    inline double width  () const { return _p2.x - _p1.x; }
-    inline double height () const { return _p2.y - _p1.y; }
-    inline double depth  () const { return _p2.z - _p1.z; }
-    inline double volume () const { return (_p2.x - _p1.x) * (_p2.y - _p1.y) * (_p2.z - _p1.z); }
-    double area(int axis) const;
-    BBox3D overlap(const BBox3D& box) const;
-    BBox3D inclusive(const BBox3D& box) const;
-    bool contains(const Point3D& point) const;
-
-    std::string dump() const;
-
-  private:
-
-    Point3D _p1; ///< bottom-left point coordinate (x1,y1,z1) where x1<x2 and y1<y2 and z1<z2
-    Point3D _p2; ///< top-right point coordinate (x2,y2,z2) where x1<x2 and y1<y2 and z1<z2
-  };
 }
 #endif
 /** @} */ // end of doxygen group
