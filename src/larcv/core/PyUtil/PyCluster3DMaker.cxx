b8899b880c9a118d6989e05b24d313e1e4f4cc5c
Corey Adams <corey.adams@anl.gov>
2019-04-11 10:37:34 -0500

    Commenting out much of pyutil.

--- src/larcv/core/PyUtil/PyCluster3DMaker.cxx
+++ src/larcv/core/PyUtil/PyCluster3DMaker.cxx
@@ -1,44 +1,44 @@
-#ifndef __PYCluster3DMAKER_CXX__
-#define __PYCluster3DMAKER_CXX__
+// #ifndef __PYCluster3DMAKER_CXX__
+// #define __PYCluster3DMAKER_CXX__
 
-#include "PyCluster3DMaker.h"
-#include "larcv/core/DataFormat/EventVoxel3D.h"
+// #include "PyCluster3DMaker.h"
+// #include "larcv/core/DataFormat/EventVoxel3D.h"
 
-namespace larcv {
+// namespace larcv {
 
-	static PyCluster3DMakerProcessFactory __global_PyCluster3DMakerProcessFactory__;
+// 	static PyCluster3DMakerProcessFactory __global_PyCluster3DMakerProcessFactory__;
 
-	PyCluster3DMaker::PyCluster3DMaker(const std::string name)
-		: ProcessBase(name)
-	{}
+// 	PyCluster3DMaker::PyCluster3DMaker(const std::string name)
+// 		: ProcessBase(name)
+// 	{}
 
-	void PyCluster3DMaker::configure(const PSet& cfg)
-	{ _producer_name = cfg.get<std::string>("Cluster3DProducer"); }
+// 	void PyCluster3DMaker::configure(const PSet& cfg)
+// 	{ _producer_name = cfg.get<std::string>("Cluster3DProducer"); }
 
-	void PyCluster3DMaker::initialize()
-	{
-		_run = _subrun = _event = kINVALID_SIZE;
-		_cluster3d_v.clear_data();
-		_meta.clear();
-	}
+// 	void PyCluster3DMaker::initialize()
+// 	{
+// 		_run = _subrun = _event = kINVALID_SIZE;
+// 		_cluster3d_v.clear_data();
+// 		_meta.clear();
+// 	}
 
-        void PyCluster3DMaker::append_ndarray(PyObject* img)
-	{
-		LARCV_INFO() << "Appending an Cluster3D (current size=" << _cluster3d_v.size() << ")" << std::endl;
-		_cluster3d_v.emplace(std::move(as_tensor3d(img)));
-	}
+//         void PyCluster3DMaker::append_ndarray(PyObject* img)
+// 	{
+// 		LARCV_INFO() << "Appending an Cluster3D (current size=" << _cluster3d_v.size() << ")" << std::endl;
+// 		_cluster3d_v.emplace(std::move(as_tensor3d(img)));
+// 	}
 
-	bool PyCluster3DMaker::process(IOManager& mgr)
-	{
-	        auto& ev_output = mgr.get_data<larcv::EventClusterVoxel3D>(_producer_name);
-		ev_output.emplace(std::move(_cluster3d_v),_meta);
-		_cluster3d_v.clear_data();
-		_meta.clear();
-		return true;
-	}
+// 	bool PyCluster3DMaker::process(IOManager& mgr)
+// 	{
+// 	        auto& ev_output = mgr.get_data<larcv::EventClusterVoxel3D>(_producer_name);
+// 		ev_output.emplace(std::move(_cluster3d_v),_meta);
+// 		_cluster3d_v.clear_data();
+// 		_meta.clear();
+// 		return true;
+// 	}
 
-	void PyCluster3DMaker::finalize()
-	{}
+// 	void PyCluster3DMaker::finalize()
+// 	{}
 
-}
-#endif
+// }
+// #endif
--- src/larcv/core/PyUtil/PyImageMaker.h
+++ src/larcv/core/PyUtil/PyImageMaker.h
@@ -1,80 +1,80 @@
-/**
- * \file PyImageMaker.h
- *
- * \ingroup Package_Name
- * 
- * \brief Class def header for a class PyImageMaker
- *
- * @author drinkingkazu
- */
+// /**
+//  * \file PyImageMaker.h
+//  *
+//  * \ingroup Package_Name
+//  * 
+//  * \brief Class def header for a class PyImageMaker
+//  *
+//  * @author drinkingkazu
+//  */
 
-/** \addtogroup Package_Name
+// /** \addtogroup Package_Name
 
-    @{*/
-#ifndef __PYIMAGEMAKER_H__
-#define __PYIMAGEMAKER_H__
+//     @{*/
+// #ifndef __PYIMAGEMAKER_H__
+// #define __PYIMAGEMAKER_H__
 
-#include "larcv/core/Processor/ProcessBase.h"
-#include "larcv/core/Processor/ProcessFactory.h"
-#include "PyUtils.h"
-#include "larcv/core/DataFormat/ImageMeta.h"
-namespace larcv {
+// #include "larcv/core/Processor/ProcessBase.h"
+// #include "larcv/core/Processor/ProcessFactory.h"
+// #include "PyUtils.h"
+// #include "larcv/core/DataFormat/ImageMeta.h"
+// namespace larcv {
 
-  /**
-     \class ProcessBase
-     User defined class PyImageMaker ... these comments are used to generate
-     doxygen documentation!
-  */
-  class PyImageMaker : public ProcessBase {
+//   /**
+//      \class ProcessBase
+//      User defined class PyImageMaker ... these comments are used to generate
+//      doxygen documentation!
+//   */
+//   class PyImageMaker : public ProcessBase {
 
-  public:
+//   public:
     
-    /// Default constructor
-    PyImageMaker(const std::string name="PyImageMaker");
+//     /// Default constructor
+//     PyImageMaker(const std::string name="PyImageMaker");
     
-    /// Default destructor
-    ~PyImageMaker(){}
+//     /// Default destructor
+//     ~PyImageMaker(){}
 
-    void configure(const PSet&);
+//     void configure(const PSet&);
 
-    void initialize();
+//     void initialize();
 
-    bool process(IOManager& mgr);
+//     bool process(IOManager& mgr);
 
-    void finalize();
+//     void finalize();
 
-    void append_ndarray(PyObject* img);
+//     void append_ndarray(PyObject* img);
 
-    void append_ndarray_meta(PyObject* img, const ImageMeta& meta);
+//     void append_ndarray_meta(PyObject* img, const ImageMeta& meta);
 
-    void set_id(size_t run, size_t subrun, size_t event)
-    { _run = run; _subrun = subrun; _event = event; }
+//     void set_id(size_t run, size_t subrun, size_t event)
+//     { _run = run; _subrun = subrun; _event = event; }
 
-  private:
+//   private:
 
-    std::vector<larcv::Image2D> _image_v;
+//     std::vector<larcv::Image2D> _image_v;
 
-    std::string _producer_name;
+//     std::string _producer_name;
 
-    size_t _run, _subrun, _event;
-  };
+//     size_t _run, _subrun, _event;
+//   };
 
-  /**
-     \class larcv::PyImageMakerFactory
-     \brief A concrete factory class for larcv::PyImageMaker
-  */
-  class PyImageMakerProcessFactory : public ProcessFactoryBase {
-  public:
-    /// ctor
-    PyImageMakerProcessFactory() { ProcessFactory::get().add_factory("PyImageMaker",this); }
-    /// dtor
-    ~PyImageMakerProcessFactory() {}
-    /// creation method
-    ProcessBase* create(const std::string instance_name) { return new PyImageMaker(instance_name); }
-  };
+//   /**
+//      \class larcv::PyImageMakerFactory
+//      \brief A concrete factory class for larcv::PyImageMaker
+//   */
+//   class PyImageMakerProcessFactory : public ProcessFactoryBase {
+//   public:
+//     /// ctor
+//     PyImageMakerProcessFactory() { ProcessFactory::get().add_factory("PyImageMaker",this); }
+//     /// dtor
+//     ~PyImageMakerProcessFactory() {}
+//     /// creation method
+//     ProcessBase* create(const std::string instance_name) { return new PyImageMaker(instance_name); }
+//   };
 
-}
+// }
 
-#endif
-/** @} */ // end of doxygen group 
+// #endif
+// /** @} */ // end of doxygen group 
 
