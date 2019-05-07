2dd409fce5f540757015173561c0fb2658acbad5
Corey Adams <corey.adams@anl.gov>
2019-05-07 16:53:31 -0500

    Update the build systems a little

---
+++ larcv/__init__.py
@@ -0,0 +1,12 @@
+from . import base
+from . import dataformat
+from . import processor
+from . import pyutil
+
+import os
+
+def get_includes():
+    return os.path.dirname(__file__) + "/include/"
+
+def get_lib_dir():
+    return os.path.dirname(__file__) + "/lib/"
