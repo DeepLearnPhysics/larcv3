8a560dcc7a772db46f3a9fd78a9331b66036d4f0
Corey Adams <corey.adams@anl.gov>
2019-05-28 21:28:28 -0500

    This adds exception handling in swig wrapped classes.  Previous behavior gave an unhandled exception leading to a core dump.  New behavior allows python to create an exception and throw it in a Pythonic way.
    For running unit tests, this means that C++ exceptions will cause individual tests to fail rather than the entire testing suite to fall over.  I like to think this is an improvement.
    
    This closes issue #4.

--- tests/larcv/core/base/test_larbys.py
+++ tests/larcv/core/base/test_larbys.py
@@ -7,3 +7,13 @@ def test_larbys():
     lib = larcv.larbys()
     # Config manager is deliberately excluded:
     # lib = base.ConfigManager()
+
+def test_larbys_exception():
+    from larcv import larcv
+    # Creating a PSet object without a name will cause an exception.
+    # We can catch that and verify we're getting a larbys exception:
+    try:
+        pset = larcv.PSet()
+        return False
+    except:
+        return True
