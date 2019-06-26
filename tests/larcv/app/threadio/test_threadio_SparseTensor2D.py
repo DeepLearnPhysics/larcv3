9cc204619d1e7a876c95e9272b397abbee61d260
Marco Del Tutto <marco.deltutto@gmail.com>
2019-06-26 10:23:53 -0400

    Addind distributed tests for all threadio tests

--- tests/larcv/app/threadio/test_threadio_SparseTensor2D.py
+++ tests/larcv/app/threadio/test_threadio_SparseTensor2D.py
@@ -3,7 +3,7 @@ import unittest
 import random
 import uuid
 
-from larcv import larcv, data_generator, larcv_interface
+from larcv import larcv, data_generator, larcv_interface, distributed_larcv_interface
 
 from collections import OrderedDict
 
@@ -92,7 +92,71 @@ def test_sparsetensor2d_threadio(tmpdir, num_threads, num_storage, make_copy, ba
         assert(data['label'].shape[0] == batch_size)
 
 
+@pytest.mark.distributed_test
+@pytest.mark.parametrize('num_storage', [1,2])
+@pytest.mark.parametrize('num_threads', [1,2])
+@pytest.mark.parametrize('make_copy', [True, False])
+@pytest.mark.parametrize('batch_size', [4])
+@pytest.mark.parametrize('n_projections', [1,2])
+def test_sparsetensor2d_threadio_distributed(tmpdir, num_threads, num_storage, make_copy, batch_size, n_projections, n_reads=10):
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+
+    root_rank = comm.Get_size() - 1
+
+    threadio_name = "threadio_{}".format(uuid.uuid4())
+
+
+    # First, create a file to write to:
+    file_name = str(tmpdir + "/test_threadio_sparsetensor2d_{}.h5".format(threadio_name))
+
+    # Next, write some sparsetensor2ds to that file:
+    if (comm.Get_rank() == root_rank):
+        create_sparsetensor2d_file(file_name, rand_num_events=25, n_projections=n_projections)
+
+
+    # Generate a config for this 
+    channels = list(range(n_projections))
+    config_contents = thread_io_sparsetensor2d_cfg_template.format(
+        name        = threadio_name,
+        input_files = file_name,
+        num_threads = num_threads,
+        num_storage = num_storage,
+        producer    = "test",
+        channels    = channels,
+        )
+
+    config_file = tmpdir + "/test_threadio_sparsetensor2d_{}.cfg".format(threadio_name) 
+
+    with open(str(config_file), 'w') as _f:
+        _f.write(config_contents)
+
+    # Prepare data managers:
+    io_config = {
+        'filler_name' : threadio_name,
+        'filler_cfg'  : str(config_file),
+        'verbosity'   : 3,
+        'make_copy'   : make_copy
+    }
+
+    data_keys = OrderedDict({
+        'label': 'test_{}'.format(threadio_name), 
+        })
+
+
+
+    li = distributed_larcv_interface.larcv_interface(root=root_rank)
+    li.prepare_manager('primary', io_config, batch_size, data_keys)
+
+
+    for i in range(n_reads):
+        data = li.fetch_minibatch_data('primary')
+        assert(data['label'].shape[0] == batch_size/comm.Get_size())
+
+
 if __name__ == "__main__":
     test_sparsetensor2d_threadio("./", num_threads=1, num_storage=1, make_copy=False, batch_size=2, n_projections=1, n_reads=10)
     test_sparsetensor2d_threadio("./", num_threads=1, num_storage=2, make_copy=False, batch_size=2, n_projections=2, n_reads=10)
+    # test_sparsetensor2d_threadio_distributed("./", num_threads=1, num_storage=2, make_copy=False, batch_size=2, n_projections=2, n_reads=10)
     print("Success")
--- tests/larcv/app/threadio/test_threadio_SparseTensor3D.py
+++ tests/larcv/app/threadio/test_threadio_SparseTensor3D.py
@@ -3,7 +3,7 @@ import unittest
 import random
 import uuid
 
-from larcv import larcv, data_generator, larcv_interface
+from larcv import larcv, data_generator, larcv_interface, distributed_larcv_interface
 
 from collections import OrderedDict
 
@@ -91,7 +91,68 @@ def test_sparsetensor3d_threadio(tmpdir, num_threads, num_storage, make_copy, ba
         assert(data['label'].shape[0] == batch_size)
 
 
+@pytest.mark.distributed_test
+@pytest.mark.parametrize('num_storage', [1,2])
+@pytest.mark.parametrize('num_threads', [1,2])
+@pytest.mark.parametrize('make_copy', [True, False])
+@pytest.mark.parametrize('batch_size', [4])
+def test_sparsetensor3d_threadio_distributed(tmpdir, num_threads, num_storage, make_copy, batch_size, n_reads=10):
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+
+    root_rank = comm.Get_size() - 1
+
+    threadio_name = "threadio_{}".format(uuid.uuid4())
+
+
+    # First, create a file to write to:
+    file_name = str(tmpdir + "/test_threadio_sparsetensor3d_{}.h5".format(threadio_name))
+
+    # Next, write some sparsetensor3ds to that file:
+    if (comm.Get_rank() == root_rank):
+        create_sparsetensor3d_file(file_name, rand_num_events=25)
+
+
+    # Generate a config for this 
+    config_contents = thread_io_sparsetensor3d_cfg_template.format(
+        name        = threadio_name,
+        input_files = file_name,
+        num_threads = num_threads,
+        num_storage = num_storage,
+        producer    = "test",
+        )
+
+    config_file = tmpdir + "/test_threadio_sparsetensor3d_{}.cfg".format(threadio_name) 
+
+    with open(str(config_file), 'w') as _f:
+        _f.write(config_contents)
+
+    # Prepare data managers:
+    io_config = {
+        'filler_name' : threadio_name,
+        'filler_cfg'  : str(config_file),
+        'verbosity'   : 3,
+        'make_copy'   : make_copy
+    }
+
+    data_keys = OrderedDict({
+        'label': 'test_{}'.format(threadio_name), 
+        })
+
+
+
+    li = distributed_larcv_interface.larcv_interface(root=root_rank)
+    li.prepare_manager('primary', io_config, batch_size, data_keys)
+
+
+    for i in range(n_reads):
+        data = li.fetch_minibatch_data('primary')
+        assert(data['label'].shape[0] == batch_size/comm.Get_size())
+
+
 if __name__ == "__main__":
     test_sparsetensor3d_threadio("./", num_threads=1, num_storage=1, make_copy=False, batch_size=2,  n_reads=10)
     test_sparsetensor3d_threadio("./", num_threads=1, num_storage=2, make_copy=False, batch_size=2,  n_reads=10)
+    # test_sparsetensor3d_threadio_distributed("./", num_threads=1, num_storage=2, make_copy=False, batch_size=2,  n_reads=10)
     print("Success")
