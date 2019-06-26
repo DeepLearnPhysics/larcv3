d7ba8a415ce08bd203ae930f0b530ae4003880f5
Marco Del Tutto <marco.deltutto@gmail.com>
2019-06-26 14:09:34 -0400

    Keeping only one test for distributed

--- tests/larcv/app/threadio/test_threadio_Image2D.py
+++ tests/larcv/app/threadio/test_threadio_Image2D.py
@@ -96,70 +96,70 @@ def test_image2d_threadio(tmpdir, num_threads, num_storage, make_copy, batch_siz
     li.stop()
 
 
-@pytest.mark.distributed_test
-@pytest.mark.parametrize('num_storage', [1, 2])
-@pytest.mark.parametrize('num_threads', [1, 2])
-@pytest.mark.parametrize('make_copy', [True, False])
-@pytest.mark.parametrize('batch_size', [2, 4])
-@pytest.mark.parametrize('n_projections', [1, 2])
-def test_image2d_threadio_distributed(tmpdir, num_threads, num_storage, make_copy, batch_size, n_projections, n_reads=10):
+# @pytest.mark.distributed_test
+# @pytest.mark.parametrize('num_storage', [1, 2])
+# @pytest.mark.parametrize('num_threads', [1, 2])
+# @pytest.mark.parametrize('make_copy', [True, False])
+# @pytest.mark.parametrize('batch_size', [2, 4])
+# @pytest.mark.parametrize('n_projections', [1, 2])
+# def test_image2d_threadio_distributed(tmpdir, num_threads, num_storage, make_copy, batch_size, n_projections, n_reads=10):
 
-    from mpi4py import MPI
-    comm = MPI.COMM_WORLD
+#     from mpi4py import MPI
+#     comm = MPI.COMM_WORLD
 
-    root_rank = comm.Get_size() - 1
+#     root_rank = comm.Get_size() - 1
 
-    threadio_name = "threadio_{}".format(uuid.uuid4())
+#     threadio_name = "threadio_{}".format(uuid.uuid4())
 
-    # First, create a file to write to:
-    file_name = str(tmpdir + "/test_threadio_image2ds_{}.h5".format(threadio_name))
+#     # First, create a file to write to:
+#     file_name = str(tmpdir + "/test_threadio_image2ds_{}.h5".format(threadio_name))
 
-    random_file_name = str(tmpdir + "/test_write_read_image2d.h5")
+#     random_file_name = str(tmpdir + "/test_write_read_image2d.h5")
 
-    # Next, write some image2ds to that file:
-    if (comm.Get_rank() == root_rank):
-        create_image2d_file(file_name, rand_num_events=25, n_projections=n_projections)
+#     # Next, write some image2ds to that file:
+#     if (comm.Get_rank() == root_rank):
+#         create_image2d_file(file_name, rand_num_events=25, n_projections=n_projections)
 
-    channels = list(range(n_projections))
+#     channels = list(range(n_projections))
 
-    # Generate a config for this 
-    config_contents = thread_io_image2d_cfg_template.format(
-        name        = threadio_name,
-        input_files = file_name,
-        num_threads = num_threads,
-        num_storage = num_storage,
-        producer    = "test",
-        channels    = channels,
-        )
+#     # Generate a config for this 
+#     config_contents = thread_io_image2d_cfg_template.format(
+#         name        = threadio_name,
+#         input_files = file_name,
+#         num_threads = num_threads,
+#         num_storage = num_storage,
+#         producer    = "test",
+#         channels    = channels,
+#         )
 
-    config_file = tmpdir + "/test_threadio_image2ds_{}.cfg".format(threadio_name) 
+#     config_file = tmpdir + "/test_threadio_image2ds_{}.cfg".format(threadio_name) 
 
-    with open(str(config_file), 'w') as _f:
-        _f.write(config_contents)
+#     with open(str(config_file), 'w') as _f:
+#         _f.write(config_contents)
 
-    # Prepare data managers:
-    io_config = {
-        'filler_name' : threadio_name,
-        'filler_cfg'  : str(config_file),
-        'verbosity'   : 3,
-        'make_copy'   : make_copy
-    }
+#     # Prepare data managers:
+#     io_config = {
+#         'filler_name' : threadio_name,
+#         'filler_cfg'  : str(config_file),
+#         'verbosity'   : 3,
+#         'make_copy'   : make_copy
+#     }
 
-    data_keys = OrderedDict({
-        'label': 'test_{}'.format(threadio_name), 
-        })
-
-
-
-    li = distributed_larcv_interface.larcv_interface(root=root_rank)
-    li.prepare_manager('primary', io_config, batch_size, data_keys)
+#     data_keys = OrderedDict({
+#         'label': 'test_{}'.format(threadio_name), 
+#         })
 
 
-    for i in range(n_reads):
-        data = li.fetch_minibatch_data('primary')
-        assert(data['label'].shape[0] == batch_size/comm.Get_size())
 
-    li.stop()
+#     li = distributed_larcv_interface.larcv_interface(root=root_rank)
+#     li.prepare_manager('primary', io_config, batch_size, data_keys)
+
+
+#     for i in range(n_reads):
+#         data = li.fetch_minibatch_data('primary')
+#         assert(data['label'].shape[0] == batch_size/comm.Get_size())
+
+#     li.stop()
 
 if __name__ == "__main__":
     test_image2d_threadio("./", num_threads=1, num_storage=1, make_copy=False, batch_size=2, n_projections=1, n_reads=10)
