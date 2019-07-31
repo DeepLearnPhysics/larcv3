1903bb78ee6364e7e3cd6faa8e0d6b37560e902e
Corey Adams <corey.adams@anl.gov>
2019-07-31 09:38:06 -0500

    Split batch_pydata into its own class.  Initial push of queueloader, entirely untested.

---
+++ larcv/batch_pydata.py
@@ -0,0 +1,81 @@
+from . import larcv
+import sys,time,os,signal
+import numpy as np
+
+class batch_pydata(object):
+
+   _storage_id = -1
+   _dtype      = None
+   _npy_data   = None
+   _dim_data   = None
+   _time_copy  = 0
+   _time_reshape = 0
+   _make_copy  = False
+
+   def __init__(self,dtype):
+      self._storage_id = -1
+      self._dtype = dtype
+      self._npy_data = None
+      self._dim_data = None
+      self._time_copy = None
+      self._time_reshape = None
+      
+   def batch_data_size(self):
+      dsize=1
+      for v in self._dim_data: dsize *= v
+      return dsize
+
+   def dtype(self): return self._dtype
+   def data(self): return self._npy_data
+   def dim(self):  return self._dim_data
+   def time_copy(self): return self._time_copy
+   def time_reshape(self): return self._time_reshape
+
+
+   def set_data(self,storage_id,larcv_batchdata):
+      self._storage_id = storage_id
+      dim = larcv_batchdata.dim()
+
+      # set dimension
+      if self._dim_data is None:
+         self._dim_data = np.array([dim[i] for i in range(len(dim))]).astype(np.int32)
+
+      else:
+         if not len(self._dim_data) == len(dim):
+            sys.stderr.write('Dimension array length changed (%d => %d)\n' % (len(self._dim_data),len(dim)))
+            raise TypeError
+         for i in range(len(self._dim_data)):
+            if not self._dim_data[i] == dim[i]:
+               sys.stderr.write('%d-th dimension changed (%d => %d)\n' % (i,self._dim_data[i],dim[i]))
+               raise ValueError
+
+      # copy data into numpy array
+      ctime = time.time()
+      if self._make_copy:
+         if self._npy_data is None:
+            # self._npy_data = np.array(larcv_batchdata.data())
+            # print("Array type: ", self._npy_data.dtype)
+            # Create an array to hold the data if it does not exits:
+            self._npy_data = np.ndarray(shape=(larcv_batchdata.data_size()), dtype=self._dtype)
+         self._npy_data = self._npy_data.reshape(self.batch_data_size())
+         if self._dtype == "float32":
+            larcv.copy_array_float(self._npy_data,larcv_batchdata.data())
+         elif self._dtype == "float64":
+            larcv.copy_array_double(self._npy_data,larcv_batchdata.data())
+      else:
+         if self._dtype == "int":
+            self._npy_data = larcv.as_ndarray_int(larcv_batchdata.data())
+         elif self._dtype == "uint":
+            self._npy_data = larcv.as_ndarray_uint(larcv_batchdata.data())
+         elif self._dtype == "float32":
+            self._npy_data = larcv.as_ndarray_float(larcv_batchdata.data())
+         elif self._dtype == "float64":
+            self._npy_data = larcv.as_ndarray_double(larcv_batchdata.data())
+      self._time_copy = time.time() - ctime
+
+
+      ctime = time.time()
+      self._npy_data = self._npy_data.reshape(self._dim_data[0], int(self.batch_data_size()/self._dim_data[0]))
+      self.time_data_conv = time.time() - ctime
+
+      return
--- larcv/queueloader.py
+++ larcv/queueloader.py
@@ -1,9 +1,142 @@
-from . import larcv
 import sys,time,os,signal
-import numpy as np
-from .batch_pydata import batch_pydata
+import numpy
 
-class larcv_threadio (object):
+from . import larcv
+from . batch_pydata import batch_pydata
+
+class queue_interface(object):
+
+
+     def __init__(self,verbose=False):
+        '''init function
+        
+        Not much to store here, just a dict of dataloaders and the keys to access their data.
+        '''
+        object.__init__(self)
+        self._dataloaders = {}
+        self._data_keys   = {}
+        self._dims        = {}
+        self._verbose     = verbose
+
+    def prepare_manager(self, mode, io_config, minibatch_size, data_keys):
+        '''Prepare a manager for io
+        
+        Creates an instance of larcv_threadio for a particular file to read.
+        
+        Arguments:
+            mode {str} -- The mode of training to store this threadio under (typically "train" or "TEST" or similar)
+            io_config {dict} -- the io config dictionary.  Required keys are: 'filler_name', 'verbosity', and 'filler_cfg'
+            data_keys_override {dict} -- If desired, you can override the keys for dataacces,
+        
+        Raises:
+            Exception -- [description]
+        '''
+
+
+        if mode in self._dataloaders:
+            raise Exception("Can not prepare manager for mode {}, already exists".format(mode))
+
+        # Check that the required keys are in the io config:
+        for req in ['filler_name', 'verbosity', 'filler_cfg']:
+            if req not in io_config:
+                raise Exception("io_config for mode {} is missing required key {}".format(mode, req))
+
+
+        start = time.time()
+
+        # Initialize and configure a manager:
+        io = larcv_queueio()
+        io.configure(io_config)
+        io.start_manager(minibatch_size)
+        self._dataloaders.update({mode : io})
+        # Force storing here, since it's not configurable on the first read.
+        self.next(mode)
+
+        while io.is_reading():
+            time.sleep(0.01)
+
+        # Save the manager
+
+        # Store the keys for accessing this datatype:
+        self._data_keys[mode] = data_keys
+    
+        # Read and save the dimensions of the data:
+        self._dims[mode] = {}
+        for key in self._data_keys[mode]:
+            self._dims[mode][key] = self._dataloaders[mode].fetch_data(self._data_keys[mode][key]).dim()
+
+        end = time.time()
+
+        # Print out how long it took to start IO:
+        if self._verbose:
+            sys.stdout.write("Time to start {0} IO: {1:.2}s\n".format(mode, end - start))
+
+
+        return
+
+
+    def next(self, mode):
+        self._dataloaders[mode].next(store_event_ids=True, store_entries=True)
+
+
+    def fetch_minibatch_data(self, mode, fetch_meta_data=False):
+        # Return a dictionary object with keys 'image', 'label', and others as needed
+        # self._dataloaders['train'].fetch_data(keyword_label).dim() as an example
+        
+        
+        while self._dataloaders[mode].is_reading():
+            time.sleep(0.01)
+
+        this_data = {}
+        for key in self._data_keys[mode]:
+            this_data[key] = self._dataloaders[mode].fetch_data(self._data_keys[mode][key]).data()
+            this_data[key] = numpy.reshape(this_data[key], self._dims[mode][key])
+
+        if fetch_meta_data:
+            this_data['entries'] = self._dataloaders[mode].fetch_entries()
+            this_data['event_ids'] = self._dataloaders[mode].fetch_event_ids()
+        
+
+        return this_data
+
+    def fetch_minibatch_dims(self, mode):
+        # Return a dictionary object with keys 'image', 'label', and others as needed
+        # self._dataloaders['train'].fetch_data(keyword_label).dim() as an example
+        return self._dims[mode]
+
+    # def is_active(self):
+    #     _is_active = False
+    #     for mode in self._dataloaders:
+    #         _is_active = _is_active and self._dataloaders[mode].
+
+    def stop(self):
+
+        for mode in self._dataloaders:
+            while self._dataloaders[mode].is_reading():
+                time.sleep(0.01)
+            self._dataloaders[mode].stop_manager()
+
+        if self._writer is not None:
+            self._writer.finalize()
+
+    def size(self, mode):
+        # return the number of images in the specified mode:
+        return self._dataloaders[mode].fetch_n_entries()
+
+    def ready(self, mode):
+        return self._dataloaders[mode].ready()
+
+
+    def write_output(self, data, datatype, producer, entries, event_ids):
+        if self._writer is None:
+            raise Exception("Trying to write data with no writer configured.  Abort!")
+
+
+        self._writer.write(data=data, datatype=datatype, producer=producer, entries=entries, event_ids=event_ids)
+
+        return
+
+class larcv_queueio (object):
 
    _instance_m={}
 
@@ -26,9 +159,9 @@ class larcv_threadio (object):
       self._current_storage_id = -1
       self._current_storage_state = -1
       self._storage = {}
-      self._tree_entries = None
+      self._event_entries = None
       self._event_ids = None
-      self._start_entry = None
+      self._next_entries = []
       self._entry_skip = None
 
    def reset(self):
@@ -55,7 +188,7 @@ class larcv_threadio (object):
          return
       self._name = cfg['filler_name']         
 
-      # get ThreadProcessor config file
+      # get QueueProcessor config file
       self._cfg_file = cfg['filler_cfg']
       if not self._cfg_file or not os.path.isfile(self._cfg_file):
          sys.stderr.write('filler_cfg file does not exist: %s\n' % self._cfg_file)
@@ -66,17 +199,9 @@ class larcv_threadio (object):
          self._verbose = bool(cfg['verbosity'])
 
       # configure thread processor
-      self._proc = larcv.ThreadProcessor(self._name)
+      self._proc = larcv.QueueProcessor(self._name)
 
       self._proc.configure(self._cfg_file)
-
-      # Set the start entry
-      if self._start_entry is not None:
-         self._proc.set_start_entry(self._start_entry)
-
-      if self._entry_skip is not None:
-         self._proc.set_entries_skip(self._entry_skip)
-
 
       # fetch batch filler info
       self._storage = {}
@@ -92,7 +217,7 @@ class larcv_threadio (object):
       # register *this* instance
       self.__class__._instance_m[self._name] = self
 
-   def set_start_entry(self, start_entry):
+   def set_next_entry(self, start_entry):
       self._start_entry = start_entry
 
    def set_entry_skip(self, entry_skip):
@@ -138,7 +263,7 @@ class larcv_threadio (object):
       self._proc.release_data()
       self._current_storage_id=-1
       self._current_storage_state = -1
-      self._tree_entries = None
+      self._event_entries = None
       self._event_ids = None
 
    def manager_started(self):
@@ -213,8 +338,8 @@ class larcv_threadio (object):
          batch_data = factory.get_storage(name).get_batch(next_storage_id)
          storage.set_data(next_storage_id, batch_data)
 
-      if not store_entries: self._tree_entries = None
-      else: self._tree_entries = larcv.VectorOfSizet(self._proc.processed_entries(next_storage_id))
+      if not store_entries: self._event_entries = None
+      else: self._event_entries = larcv.VectorOfSizet(self._proc.processed_entries(next_storage_id))
 
       if not store_event_ids: self._event_ids = None
       else: self._event_ids = larcv.VectorOfEventID(self._proc.processed_events(next_storage_id))
@@ -234,7 +359,7 @@ class larcv_threadio (object):
       return self._event_ids
 
    def fetch_entries(self):
-      return self._tree_entries
+      return self._event_entries
 
    def fetch_n_entries(self):
       return self._proc.get_n_entries()
