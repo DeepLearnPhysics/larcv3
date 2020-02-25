9654961066b8e49f52f81d69ad0a404d9e935051
Corey Adams <corey.adams@anl.gov>
2020-02-25 13:50:00 -0600

    Update vector bindings

--- src/larcv3/larcv.cxx
+++ src/larcv3/larcv.cxx
@@ -3,6 +3,20 @@
 #include "larcv3/core/processor/processor.h"
 #include "larcv3/app/queueio/queueio.h"
 
+
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::SparseTensor<2> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::SparseTensor<3> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <1> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <2> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <3> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::Tensor      <4> >);
+
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <1> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <2> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <3> >);
+PYBIND11_MAKE_OPAQUE(std::vector<larcv3::ImageMeta   <4> >);
+
+
 PYBIND11_MODULE(pylarcv, m) {
   init_base(m);
   init_dataformat(m);
