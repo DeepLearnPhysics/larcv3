c5b35dbe71953fc2bfff5931de8ee6ef114b985d
Corey <>
2022-02-02 14:14:36 -0600

    Removing weird include directory

--- larcv/include/larcv3/core/processor/ProcessorTypes.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LARCV3PROCESSOR_PROCESSORTYPES_H__
-#define __LARCV3PROCESSOR_PROCESSORTYPES_H__
-
-namespace larcv3 {
-
-  typedef size_t ProcessID_t; ///< A unique identifier for an instantiated processes
-
-}
-#endif
