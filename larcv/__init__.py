dead45946125764f8aef704d1f82d2ab5e4698a5
Corey <>
2022-09-14 15:33:01 -0500

    Restore test-on-push actions.  Change import path of ConfigBuilder

--- larcv/__init__.py
+++ larcv/__init__.py
@@ -4,6 +4,8 @@ from . pylarcv import *
 import os
 from . _version import __version__
 
+from . config_builder import ConfigBuilder
+
 def get_includes():
     return os.path.dirname(__file__) + "/include/"
 
